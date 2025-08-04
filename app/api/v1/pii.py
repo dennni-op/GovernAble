e3a0043d1f8e06eb652453a78d16cdf92a543a75
dennni-op <dennisquansah05@gmail.com>
2025-08-04 20:41:35 +0100

    Regex logic implemented and tested

--- app/api/v1/pii.py
+++ app/api/v1/pii.py
@@ -1,10 +1,36 @@
 from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import JSONResponse
-
+import re
 router = APIRouter()
+
+def detect_pattern(text_code):
+    
+    # define regex patterns for PII data 
+    patterns = {
+        "Email": r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
+        "Phone": r"\+?[1-9]\d{1,14}",
+    }
+
+    #define findings as matches in text and add matches to list
+    findings = []
+    for label, pattern in patterns.items():
+        if re.search(pattern, text_code):
+            findings.append(label)
+    return findings
 
 @router.post("/scan")
 async def scan_file(file: UploadFile = File(...)):
+    # scans file
     content = await file.read()
-    # Placeholder scan logic
-    return JSONResponse(content={"filename": file.filename, "pii_found": False})
+    findings = []
+    count_line = 0
+    for line in content.decode().splitlines():
+        matches = detect_pattern(line)
+        count_line += 1
+        if matches:
+            findings.append({"Line Number": count_line, "Types": matches })
+    return JSONResponse(content={
+        "filename": file.filename,
+        "pii_found": bool(findings),
+        "Findings": findings
+    })
