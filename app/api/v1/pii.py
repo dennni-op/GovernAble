abcac268f3a2538bec32d672243a22fb45b20d2f
dennni-op <dennisquansah05@gmail.com>
2025-08-11 19:22:30 +0100

    Finish integration with Presidio + custom regex

--- app/api/v1/pii.py
+++ app/api/v1/pii.py
@@ -1,37 +1,59 @@
 from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import JSONResponse
+from presidio_analyzer import AnalyzerEngine
 from pprint import pprint
 import json
 import re
+
 router = APIRouter()
+analyzer = AnalyzerEngine()
+
 def detect_pattern(text_code):
-	# define regex patterns for PII data 
-	patterns = {
-		"Email": r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
-		"Phone": r"\+?[1-9]\d{1,14}"
+	# Presidio detection
+	presidio_results = analyzer.analyze(text=text_code, entities=[], language='en')
+	findings = [
+		{
+			"entity": r.entity_type,
+			"start": r.start,
+			"end": r.end,
+			"score": r.score,
+			"text": text_code[r.start:r.end]
+		}
+		for r in presidio_results
+	]
+	# Regex detection (custom patterns)
+	custom_patterns = {
+		"Credit Card Number ": r"\b(?:\d[ -]*?){16}\b",
+		"NI Number": r"\b[A-Z]{2}\d{6}\b[A-Z]{1}",
+		"NHS Number": r"\b\d{3}-\d{3}-\d{4}\b",
+		"Passport Number": r"\b[A-Z]{2}\d{6,9}\b"
 	}
-	#define findings as matches in text and add matches to list
-	findings = []
-	for label, pattern in patterns.items():
-		if re.search(pattern, text_code):
-			findings.append(label)
+	scorevalue = None  # Default score value for custom patterns
+	for label, pattern in custom_patterns.items():
+		for match in re.finditer(pattern, text_code):
+			findings.append({
+				"entity": label,
+				"start": match.start(),
+				"end": match.end(),
+				"score": scorevalue,
+				"text": match.group()
+			})
 	return findings
    
 @router.post("/scan")
 async def scan_file(file: UploadFile = File(...)):
-	# scans file
 	content = await file.read()
 	findings = []
 	count_line = 0
-	# iterate through each line in the file content
 	for line in content.decode().splitlines():
 		matches = detect_pattern(line)
 		count_line += 1
-		# if the matches (function return true) exists return line n and type
 		if matches:
-			findings.append({"Line Number": count_line, "Types": matches })
+			for match in matches:
+				match["line_number"] = count_line
+				findings.append(match)
 	return JSONResponse(content={
 		"filename": file.filename,
 		"pii_found": bool(findings),
-		"Findings": findings
+		"findings": findings
 	})
--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from app.api.v1 import pii
 
 app = FastAPI(title="AI Security API")
 
-app.include_router(pii.router, prefix="/api/v1/pii", tags=["PII Detection"])
+app.include_router(pii.router, prefix="/api/v1/pii.py", tags=["PII Detection"])
